2f9523d0a3f277991e634e6b9fd7255e840bc86f
tasnimahmed11 <ta1743@nyu.edu>
2024-11-25 18:35:29 +0400

    updates

---
+++ assets/js/blur.js
@@ -0,0 +1,13 @@
+myBlurFunction = function(state) {
+    /* state can be 1 or 0 */
+    var containerElement = document.getElementById('main_container');
+    var overlayEle = document.getElementById('overlay');
+
+    if (state) {
+        overlayEle.style.display = 'block';
+        containerElement.setAttribute('class', 'blur');
+    } else {
+        overlayEle.style.display = 'none';
+        containerElement.setAttribute('class', null);
+    }
+};
